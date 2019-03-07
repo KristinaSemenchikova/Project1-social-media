e8f12f63f859ffbbc214d00d41af6140a8a217e7
kristinahappy4ever <kristinaalekseevna@gmail.com>
2019-03-08 00:50:53 +0300

    move state to store , refactoring,bind context

--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import state from './redux/state';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { addPost } from './redux/state';
-import {updateNewPostText} from './redux/state';
-import {sendMessage} from './redux/state';
-import {updateNewMessage} from './redux/state';
-import {subscribe} from './redux/state';
-import {store} from './redux/state';
+import store from './redux/state';
 
  let rerenderEntireTree = () => {
 ReactDOM.render(<BrowserRouter>
-<App state={state} addPost={store.addPost} updateNewPostText = {store.updateNewPostText} 
-sendMessage = {store.sendMessage} updateNewMessage = {store.updateNewMessage}/>
+<App state={store.getState.call(store)} addPost={store.addPost.bind(store)} 
+updateNewPostText = {store.updateNewPostText.bind(store)} 
+sendMessage = {store.sendMessage.bind(store)} updateNewMessage = {store.updateNewMessage.bind(store)}/>
 </BrowserRouter>, document.getElementById('root'));
 }
-rerenderEntireTree(state);
+// store.subscribe method gets observer (function) as a parameter
+rerenderEntireTree(store.getState.bind(store));
 store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,68 +1,105 @@
-let rerenderEntireTree = () => {
-  console.log('State is changed')
-};
+// let rerenderEntireTree = () => {
+//   console.log('State is changed')
+// };
+// let state = {
+//   profilePage: {
+//     posts: [
+//       { id: 1, message: 'Hi,sweety', likes: 10 },
+//       { id: 2, message: 'Come and see my Instagram', likes: 17 }
+//     ],
+//     newPostText: 'Anything meow?'
+//   },
+//   dialogsPage: {
+//     dialogs: [
+//       { id: 1, name: 'Christian Bale', avatar: 'https://mfst.igromania.ru/wp-content/uploads/2016/01/bale-start.jpg' },
+//       { id: 2, name: 'Ryan Reynolds', avatar: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAykO5L.img?h=317&w=624&m=6&q=60&o=f&l=f&x=1068&y=368' },
+//       { id: 3, name: 'Ryan Gosling', avatar: 'https://cdnph.upi.com/svc/sv/upi/6241487592217/2017/1/834a9c5065f23f503e786fddcf03fb43/Ryan-Gosling-Rooney-Mara-fall-in-love-in-first-trailer-for-Song-to-Song.jpg' },
+//       { id: 4, name: 'Leonardo Dicaprio', avatar: 'https://www.radiodelta.rs/wp-content/uploads/2017/08/56d34c31c361887b278b4606.jpg' }
+//     ],
 
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'Hi,sweety', likes: 10 },
-      { id: 2, message: 'Come and see my Instagram', likes: 17 }
-    ],
-    newPostText: 'Anything meow?'
-  },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: 'Christian Bale', avatar: 'https://mfst.igromania.ru/wp-content/uploads/2016/01/bale-start.jpg' },
-      { id: 2, name: 'Ryan Reynolds', avatar: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAykO5L.img?h=317&w=624&m=6&q=60&o=f&l=f&x=1068&y=368' },
-      { id: 3, name: 'Ryan Gosling', avatar: 'https://cdnph.upi.com/svc/sv/upi/6241487592217/2017/1/834a9c5065f23f503e786fddcf03fb43/Ryan-Gosling-Rooney-Mara-fall-in-love-in-first-trailer-for-Song-to-Song.jpg' },
-      { id: 4, name: 'Leonardo Dicaprio', avatar: 'https://www.radiodelta.rs/wp-content/uploads/2017/08/56d34c31c361887b278b4606.jpg' }
-    ],
-
-    messages: [
-      { id: 1, message: 'What are you doing tonight?' },
-      { id: 2, message: 'Hi' },
-      { id: 3, message: 'Lets go to party' },
-      { id: 4, message: 'LOL' }
-    ],
-    newMessage: ''
-  },
-  sidebar: {
-    friends: [
-      { id: 1, name: 'Christian Bale', avatar: 'https://mfst.igromania.ru/wp-content/uploads/2016/01/bale-start.jpg' },
-      { id: 2, name: 'Ryan Reynolds', avatar: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAykO5L.img?h=317&w=624&m=6&q=60&o=f&l=f&x=1068&y=368' },
-      { id: 3, name: 'Ryan Gosling', avatar: 'https://cdnph.upi.com/svc/sv/upi/6241487592217/2017/1/834a9c5065f23f503e786fddcf03fb43/Ryan-Gosling-Rooney-Mara-fall-in-love-in-first-trailer-for-Song-to-Song.jpg' },
-    ]
-  }
-
-};
+//     messages: [
+//       { id: 1, message: 'What are you doing tonight?' },
+//       { id: 2, message: 'Hi' },
+//       { id: 3, message: 'Lets go to party' },
+//       { id: 4, message: 'LOL' }
+//     ],
+//     newMessage: ''
+//   },
+//   sidebar: {
+//     friends: [
+//       { id: 1, name: 'Christian Bale', avatar: 'https://mfst.igromania.ru/wp-content/uploads/2016/01/bale-start.jpg' },
+//       { id: 2, name: 'Ryan Reynolds', avatar: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAykO5L.img?h=317&w=624&m=6&q=60&o=f&l=f&x=1068&y=368' },
+//       { id: 3, name: 'Ryan Gosling', avatar: 'https://cdnph.upi.com/svc/sv/upi/6241487592217/2017/1/834a9c5065f23f503e786fddcf03fb43/Ryan-Gosling-Rooney-Mara-fall-in-love-in-first-trailer-for-Song-to-Song.jpg' },
+//     ]
+//   }
+// };
 // create store with all methods instead of functions
-export default state;
-export let store = {
+// export default state;
+ let store = {
+  _state : {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'Hi,sweety', likes: 10 },
+        { id: 2, message: 'Come and see my Instagram', likes: 17 }
+      ],
+      newPostText: 'Anything meow?'
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Christian Bale', avatar: 'https://mfst.igromania.ru/wp-content/uploads/2016/01/bale-start.jpg' },
+        { id: 2, name: 'Ryan Reynolds', avatar: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAykO5L.img?h=317&w=624&m=6&q=60&o=f&l=f&x=1068&y=368' },
+        { id: 3, name: 'Ryan Gosling', avatar: 'https://cdnph.upi.com/svc/sv/upi/6241487592217/2017/1/834a9c5065f23f503e786fddcf03fb43/Ryan-Gosling-Rooney-Mara-fall-in-love-in-first-trailer-for-Song-to-Song.jpg' },
+        { id: 4, name: 'Leonardo Dicaprio', avatar: 'https://www.radiodelta.rs/wp-content/uploads/2017/08/56d34c31c361887b278b4606.jpg' }
+      ],
+  
+      messages: [
+        { id: 1, message: 'What are you doing tonight?' },
+        { id: 2, message: 'Hi' },
+        { id: 3, message: 'Lets go to party' },
+        { id: 4, message: 'LOL' }
+      ],
+      newMessage: ''
+    },
+    sidebar: {
+      friends: [
+        { id: 1, name: 'Christian Bale', avatar: 'https://mfst.igromania.ru/wp-content/uploads/2016/01/bale-start.jpg' },
+        { id: 2, name: 'Ryan Reynolds', avatar: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AAykO5L.img?h=317&w=624&m=6&q=60&o=f&l=f&x=1068&y=368' },
+        { id: 3, name: 'Ryan Gosling', avatar: 'https://cdnph.upi.com/svc/sv/upi/6241487592217/2017/1/834a9c5065f23f503e786fddcf03fb43/Ryan-Gosling-Rooney-Mara-fall-in-love-in-first-trailer-for-Song-to-Song.jpg' },
+      ]
+    }
+  },
+  getState () {
+    return this._state;
+  },
+  _callSubscriber () {
+    console.log('State is changed')
+  },
   addPost () {
-    let newPost = { id: 5, message: state.profilePage.newPostText, likes: 9 };
-    if (state.profilePage.newPostText.length !== 0) { state.profilePage.posts.push(newPost) };
-    rerenderEntireTree();
-    state.profilePage.newPostText = "";
+    let newPost = { id: 5, message: this._state.profilePage.newPostText, likes: 9 };
+    if (this._state.profilePage.newPostText.length !== 0) { this._state.profilePage.posts.push(newPost) };
+    this._callSubscriber();
+    this._state.profilePage.newPostText = "";
   },
   updateNewPostText (newText) {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree();
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber();
   },
   
   sendMessage () {
-    let newMessage = { id: 5, message: state.dialogsPage.newMessage };
-    if (state.dialogsPage.newMessage !== 0) { state.dialogsPage.messages.push(newMessage) };
-    rerenderEntireTree();
-    state.dialogsPage.newMessage = "";
+    let newMessage = { id: 5, message: this._state.dialogsPage.newMessage };
+    if (this._state.dialogsPage.newMessage !== 0) { this._state.dialogsPage.messages.push(newMessage) };
+    this._callSubscriber();
+    this._state.dialogsPage.newMessage = "";
   },
   updateNewMessage  (messageText) {
-    state.dialogsPage.newMessage = messageText;
-    rerenderEntireTree();
+    this._state.dialogsPage.newMessage = messageText;
+    this._callSubscriber();
   },
-   subscribe  (observer) {
-    rerenderEntireTree = observer;
+   subscribe (observer) {
+    this._callSubscriber  = observer;
   }
 }
+export default store;
 // export const addPost = () => {
 //   let newPost = { id: 5, message: state.profilePage.newPostText, likes: 9 };
 //   if (state.profilePage.newPostText.length !== 0) { state.profilePage.posts.push(newPost) };
